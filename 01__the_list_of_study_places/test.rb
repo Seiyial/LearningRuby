be053adc8ba0f6444302a54854db84fbc6c08e76
Dagreattez <tessaritan@gmail.com>
2018-07-11 21:47:25 +0800

    WIP

---
+++ 01__the_list_of_study_places/test.rb
@@ -0,0 +1,41 @@
+study_areas = [
+    {
+        name: 'Yoshinoya',
+        rough_location: 'Sengkang',
+        level_of_crowdedness: 'Medium',
+        price_level: 'Intermediate',
+        spending: '6 to 10 dollars'
+    },
+    {
+        name: 'Coffeebean',
+        rough_location: 'Fernvale in Sengkang',
+        level_of_crowdedness: 'Medium',
+        price_level: 'Intermediate',
+        spending: '5 to 8 dollars'
+    },
+    {
+        name: 'Starbucks',
+        rough_location: 'Singapore Institute of Management',
+        level_of_crowdedness: 'Low',
+        price_level: 'Intermediate',
+        spending: '5 to 8 dollars'
+    },
+    {
+        name: 'Subway',
+        rough_location: 'Yishun',
+        level_of_crowdedness: 'Low',
+        price_level: 'Affordable',
+        spending: '3 to 6 dollars'
+    },
+    {
+        name: 'School Library',
+        rough_location: 'Nanyang Polytechnic',
+        level_of_crowdedness: 'Low',
+        price_level: 'Affordable',
+        spending: '3 to 6 dollars'
+    }
+]
+
+# answer = 
+
+# print answer
