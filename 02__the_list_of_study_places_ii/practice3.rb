f888641a5ce1759cfa9c219cf21d80bd79c3cf16
Seiyial <seiyial@gmail.com>
2018-07-18 20:56:56 +0800

    Add practice3 to 02

---
+++ 02__the_list_of_study_places_ii/practice3.rb
@@ -0,0 +1,21 @@
+subway_outlets = [
+	{
+		name: 'Khoo Teck Puat Hospital Subway',
+		location: 'Khoo Teck Puat Hospital, Yishun',
+		rating: '4.5/5',
+		opening: '8am ~ 1am'
+	},
+	{
+		name: 'Causeway Point Subway',
+		location: 'Causeway Point, Woodlands',
+		rating: '4/5',
+		opening: '9am ~ 10pm'
+	},
+	{
+		name: 'Compass Point Subway',
+		location: 'Compass Point, Sengkang Central',
+		rating: '4/5',
+		opening: '9am ~ 10pm'
+	}
+]
+
